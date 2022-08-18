d9b96bbb6ef47bd1bd22f565a7e52c819052d3b4
pippogit <filipscotto@gmail.com>
2022-08-18 22:03:02 +0200

    test deploy

--- next.config.js
+++ next.config.js
@@ -5,7 +5,9 @@ const isProd = process.env.NODE_ENV === "production";
 const nextConfig = {
   reactStrictMode: true,
   swcMinify: true,
-  assetPrefix: isProd ? "./" : undefined,
+  // assetPrefix: isProd ? "./" : undefined,
+  basePath: isProd ? process.env.NEXT_PUBLIC_BASE_PATH : undefined,
+  assetPrefix: isProd ? process.env.NEXT_PUBLIC_BASE_PATH : undefined,
 };
 
 module.exports = nextConfig;
